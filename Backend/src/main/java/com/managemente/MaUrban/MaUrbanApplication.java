ec781f85ab2e95f3aadd25f1b00e77482018889a
hebertberamos <hebertramos09@gmail.com>
2026-06-09 19:50:46 -0300

    starting project

---
+++ Backend/src/main/java/com/managemente/MaUrban/MaUrbanApplication.java
@@ -0,0 +1,13 @@
+package com.managemente.MaUrban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaUrbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaUrbanApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/managemente/MaUrban/MaUrbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managemente.MaUrban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaUrbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
